7230a28ce416ba1c7eed25f1b93159c42fdf2a97
kamil <kamil.majek95@gmail.com>
2022-05-05 20:46:56 +0200

    swagger open api setup

--- index.js
+++ index.js
@@ -10,17 +10,35 @@ const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
 
 const session = require("express-session");
-const { dbOptions, createUser, searchUserByAny } = require("./tools/db");
 const MySQLStore = require("express-mysql-session")(session);
 
 //create test user if doesn't exists
 
+const jsDocOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "crud sample",
+            version: "1.0.0",
+            description: "Sample of crud with using SQL, Swagger, tests, session auth and pug views"
+        },
+        servers: [
+            {
+                url: `http://${DOMAIN}${!["443", "80"].includes(PORT) ? ":" + PORT : ""}`
+            }
+        ],
+    },
+    apis: ["./routes/*.js"]
+}
 
+const swaggerOptions = swaggerJSDoc(jsDocOptions);
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerOptions))
 app.use("/", require("./routes/router"));
 
+app.listen(PORT, () => {console.log("running");})
 module.exports = app;
